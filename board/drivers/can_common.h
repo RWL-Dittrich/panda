9c2be8c0b7669618e09dd92ab271ef3a67c510cf
Robin Dittrich <r.dittrich@niverplast.com>
2026-02-19 09:55:36 +0100

    fix error

--- board/drivers/can_common.h
+++ board/drivers/can_common.h
@@ -155,8 +155,9 @@ void can_set_forwarding(uint8_t from, uint8_t to) {
 }
 #endif
 void ignition_can_hook(CANPacket_t *msg) {
+  int len = GET_LEN(msg);
+
   if (msg->bus == 0U) {
-    int len = GET_LEN(msg);
     // GM exception
     if ((msg->addr == 0x1F1U) && (len == 8)) {
       // SystemPowerMode (2=Run, 3=Crank Request)
